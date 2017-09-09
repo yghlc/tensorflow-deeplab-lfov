89efc0ef9eb2212529db942fcec9739f6e8526a0
Lingcao Huang <huanglingcao@gmail.com>
2017-09-09 14:12:06 +0800

    change learning rate

--- run_train_tensorflow-deeplab-lfov.sh
+++ run_train_tensorflow-deeplab-lfov.sh
@@ -9,6 +9,9 @@ dataDir=/home/hlc/Data/VOCdevkit/VOC2012
 #data_list=/home/hlc/Data/aws_SpaceNet/un_gz/voc_format/AOI_2_Vegas_Train/trainval_aug_path.txt
 #num_classes=2
 batchSize=16
+learning_rate=1e-5
+SNAPSHOT_DIR=snapshots_03
+
 
 pre_train_model=${HOME}/Data/deeplab/pre-train/tensorflow-deeplab-lfov/model.ckpt-init
 #pre_train_model=./snapshots/model.ckpt-3000.data-00000-of-00001
@@ -16,4 +19,4 @@ pre_train_model=${HOME}/Data/deeplab/pre-train/tensorflow-deeplab-lfov/model.ckp
 #run script with python3 and tensorflow
 #python ${train_script} --data_dir ${dataDir} --data_list ${data_list} --restore_from ${pre_train_model} --num-classes ${num_classes} --batch_size ${batchSize}   --save_num_images 5
 
-python ${train_script} --data_dir ${dataDir} --restore_from ${pre_train_model}  --batch_size ${batchSize}   --save_num_images 5 --save_pred_every 2000
+python ${train_script} --data_dir ${dataDir} --restore_from ${pre_train_model}  --batch_size ${batchSize}   --save_num_images 5 --save_pred_every 2000 --learning_rate ${learning_rate}   --snapshot_dir ${SNAPSHOT_DIR}
